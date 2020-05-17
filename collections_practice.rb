99e55cbd7a9fbc485cf6f48018ad14adcca4725b
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-17 23:43:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -68,7 +68,7 @@ def add_s(array)
   
   array.each_with_index.collect do |word, index|
     
-   if index < 1  && index > 1
+   if index < 1 || index > 1
      adding_s << word.join("s")
    else index == 1
      adding_s << word
